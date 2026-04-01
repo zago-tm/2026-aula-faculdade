e304bec32c98a1464b5f35d88a85430098151a71
Miguel Dolabella Miranda <972613261@ulife.com.br>
2026-03-31 21:48:41 -0300

    java

---
+++ aula-31-03-26/sla/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        System.out.println("Bem vindo a Faseh pobre");
+        // comentario
+        // tipos de var
+        //int, float, double, char, string
+        //int - inteiro
+        //float - real
+        //double - decimal de um numero ou mais
+        //char-  armazena um unico caracteri
+        // string- variavel de texto
+    }
+}
---
+++ aula-31-03-26/sla/src/main/java/org/example/teste2.java
@@ -0,0 +1,14 @@
+
+package org.example;
+import  java.util.Scanner;
+public class teste2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print ("insira um numero:");
+        int numero= input.nextInt();
+
+        System.out.println(numero);
+    }
+}
